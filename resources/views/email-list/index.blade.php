6da0172a9756ad57a4e65dec6527d705873f1ef1
Rafael Lunardelli <rafael@lunardelli.me>
2024-08-22 00:01:16 -0600

    criacao inicial de uma lista de email

---
+++ resources/views/email-list/index.blade.php
@@ -0,0 +1,17 @@
+<x-layouts.app>
+    <x-slot name="header">
+        <x-h2> {{ __('Email List') }} </x-h2>
+    </x-slot>
+
+    <x-card>
+        @forelse ($emailLists as $list)
+            // fazer a lista das listas
+        @empty
+            <div class="flex justify-center">
+                <x-link-button :href="route('email-list.create')">
+                    {{ __('Create your first email list') }}
+                </x-link-button>
+            </div>
+        @endforelse
+    </x-card>
+</x-layouts.app>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Http\Controllers\EmailListController;
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+Route::get('/', function () {
+    Auth::loginUsingId(1);
+
+    return to_route('dashboard');
+});
 
 Route::view('/dashboard', 'dashboard')->middleware(['auth', 'verified'])->name('dashboard');
 
@@ -11,6 +17,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/email-list', [EmailListController::class, 'index'])->name('email-list.index');
+    Route::get('/email-list/create', [EmailListController::class, 'create'])->name('email-list.create');
+    Route::post('/email-list/store', [EmailListController::class, 'store'])->name('email-list.store');
 });
 
 require __DIR__.'/auth.php';

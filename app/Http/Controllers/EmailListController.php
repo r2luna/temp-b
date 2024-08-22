dfb8cdf60ea6abf55ec16563af5ec77256074042
Rafael Lunardelli <rafael@lunardelli.me>
2024-08-22 12:06:34 -0600

    salvando emails da lista de emails

--- app/Http/Controllers/EmailListController.php
+++ app/Http/Controllers/EmailListController.php
@@ -32,10 +32,30 @@ class EmailListController extends Controller
     {
         $data = $request->validate([
             'title' => ['required', 'max:255'],
-            // 'file' => ['required', 'file'],
+            'file' => ['required', 'file', 'mimes:csv'],
         ]);
 
-        EmailList::query()->create($data);
+        $file = $request->file('file');
+        $fileHandle = fopen($file->getRealPath(), 'r');
+        $items = [];
+
+        while (($row = fgetcsv($fileHandle, null, ',')) !== false) {
+            if ($row[0] == 'Name' && $row[1] == 'Email') {
+                continue;
+            }
+
+            $items[] = [
+                'name' => $row[0],
+                'email' => $row[1],
+            ];
+        }
+
+        fclose($fileHandle);
+
+        $emailList = EmailList::query()->create([
+            'title' => $request->title,
+        ]);
+        $emailList->subscribers()->createMany($items);
 
         return to_route('email-list.index');
     }
--- resources/views/email-list/create.blade.php
+++ resources/views/email-list/create.blade.php
@@ -4,7 +4,7 @@
     </x-slot>
 
     <x-card>
-        <x-form :action="route('email-list.store')" post>
+        <x-form :action="route('email-list.store')" post enctype="multipart/form-data">
             <div>
                 <x-input-label for="title" :value="__('Title')" />
                 <x-text-input id="title" class="block mt-1 w-full" name="title" :value="old('title')" autofocus />
@@ -12,8 +12,8 @@
             </div>
 
             <div>
-                <x-input-label for="file" :value="__('File List')" />
-                <x-text-input id="file" class="block mt-1 w-full" type="file" name="file" autofocus />
+                <x-input-label for="file" :value="__('List File')" />
+                <x-text-input id="file" class="block mt-1 w-full" type="file" accept=".csv" name="file" autofocus />
                 <x-input-error :messages="$errors->get('file')" class="mt-2" />
             </div>
 

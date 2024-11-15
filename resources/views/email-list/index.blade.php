b3a86481aa865ac8ac5a3095fba3b61a761dd456
Rafael Lunardelli <rafael@lunardelli.me>
2024-11-15 10:20:58 -0700

    deletando um email-list

--- resources/views/email-list/index.blade.php
+++ resources/views/email-list/index.blade.php
@@ -22,8 +22,13 @@
                             <x-table.td class="w-1">{{ $list->id }}</x-table.td>
                             <x-table.td>{{ $list->title }}</x-table.td>
                             <x-table.td class="w-1">{{ $list->subscribers_count }}</x-table.td>
-                            <x-table.td class="w-1">
+                            <x-table.td class="w-1 flex gap-2 items-center">
                                 <x-button.link secondary :href="route('subscribers.index', $list)">Subscribers</x-button.link>
+                                <x-form delete action="{{ route('email-list.delete', ['emailList' => $list]) }}" onsubmit="return confirm('{{ __('Are you sure?') }}')">
+                                    <x-button.danger type="submit">
+                                        {{ __('Delete') }}
+                                    </x-button.danger>
+                                </x-form>
                             </x-table.td>
                         </tr>
                     @endforeach
